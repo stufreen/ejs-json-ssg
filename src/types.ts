22f6969adc44130c86bc710b9c4f088282c8eeda
Stu Freen <stu.freen@shopify.com>
2020-05-12 13:59:48 -0400

    Start adding validators

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,21 @@ export interface Config {
   contentDir: string;
   defaultLanguage: string;
 }
+
+export interface Field {
+  title?: string;
+  id: string;
+  type: string;
+  value: Field[] | string | number | boolean | null;
+}
+
+export interface SiteNode {
+  slug: string;
+  template: string;
+  visible?: boolean;
+  meta?: {
+    [key: string]: string;
+  };
+  fields?: Field[];
+  children?: SiteNode[];
+}
--- src/validateSiteFile.ts
+++ src/validateSiteFile.ts
@@ -1,5 +1,51 @@
-const validateSiteFile = (root: any, templateDir: string) => {
+import { Field, SiteNode } from './types';
+
+const isString = (value: any): boolean =>
+  typeof value === 'string' || value instanceof String;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isField(maybeField: any): maybeField is Field {
+  if (!maybeField.id) {
+    throw new Error('Invalid field: missing "id"');
+  }
+
+  if (!isString(maybeField.id)) {
+    throw new Error('Invalid field: "id" must be a string');
+  }
+
+  if (!maybeField.type) {
+    throw new Error('Invalid field: missing "type"');
+  }
+
+  if (!isString(maybeField.type)) {
+    throw new Error('Invalid field: "type" must be a string');
+  }
+
+  if (typeof maybeField.value === 'undefined') {
+    throw new Error('Invalid field: "value" is not defined');
+  }
+
+  if (
+    typeof maybeField.value !== 'boolean' &&
+    typeof maybeField.value !== 'number' &&
+    maybeField !== null &&
+    !isString(maybeField.value) &&
+    !isField(maybeField.value) // TO DO: Check if Field array, not single field
+  ) {
+    throw new Error(
+      'Invalid field: "value" can be a boolean, number, string, null or array of fields'
+    );
+  }
+
+  return true;
+}
+
+function isSiteNode(maybeNode: any): maybeNode is SiteNode {
+  return true; // TO DO: Add type checks
+}
+
+const validateSiteFile = (root: any, templateDir: string): root is SiteNode => {
   return false;
 };
 
-export default validateSiteFile
+export default validateSiteFile;
